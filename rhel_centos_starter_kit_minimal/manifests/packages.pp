6c52da9a0106b9404d40bd9a87396f6d9c0a8aa0
seif karoui <karouiseif@gmail.com>
2017-05-22 11:42:30 +0100

    added file content

--- rhel_centos_starter_kit_minimal/manifests/packages.pp
+++ rhel_centos_starter_kit_minimal/manifests/packages.pp
@@ -1,8 +1,17 @@
 class packages {
   $packages = [net-tools,vim-enhanced,nc,wireshark,tcpdump,wget,strace,lynx,sysstat,lsof,deltarpm,mlocate,bash-completion,nmap,telnet,lshw]
+  $repo_content="
+[centos_repo]
+name= cent_os repo
+baseurl=http://mirror.centos.org/centos/7/os/x86_64/
+enabled=1
+gpgchek=0
+"
   package {$packages: 
     ensure => installed,
   }
   file{ centos_repo:
+    ensure => present,
+    content => $repo_content,
   }
 }
