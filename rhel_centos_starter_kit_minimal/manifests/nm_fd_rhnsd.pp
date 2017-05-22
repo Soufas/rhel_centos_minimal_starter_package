966d59ee74cf954aad2f048c6df19c1b668f320e
seif karoui <karouiseif@gmail.com>
2017-05-22 15:31:39 +0100

    added full path to exec commands

--- rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
+++ rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
@@ -6,10 +6,10 @@ class rhel_centos_starter_kit_minimal::nm_fd_rhnsd(
 {
   if ( $disable_nm == "yes" ) {    
       exec {'disable_nm_controlled':
-      command => "echo NM_CONTROLLED=false >>/etc/sysconfig/network-scripts/ifcfg-*"
+      command => "/usr/bin/echo NM_CONTROLLED=false >>/etc/sysconfig/network-scripts/ifcfg-*"
            }
       exec {'mask_nm':
-          command => "systemctl mask NetworkManager"
+          command => "/usr/bin/systemctl mask NetworkManager"
             }
    }
 }
