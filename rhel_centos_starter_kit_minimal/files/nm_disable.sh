81578625b7a7d9844f0fa01cd70d975e12272d60
seif karoui <karouiseif@gmail.com>
2017-05-24 18:34:16 +0100

    scs

--- rhel_centos_starter_kit_minimal/files/nm_disable.sh
+++ rhel_centos_starter_kit_minimal/files/nm_disable.sh
@@ -1,9 +1,14 @@
 #!/bin/bash
+CHANGES=0
 for IFCFG in /etc/sysconfig/network-scripts/ifcfg-* ;do
 echo $IFCFG
 OCCURENCE=$(grep NM_CONTROLLED $IFCFG |wc -l)
 if [ $OCCURENCE == 0 ];
 then
 echo NM_CONTROLLED=false >> $IFCFG
+CHANGES=$[ $CHANGES + 1 ]
 fi
 done
+if [ $CHANGES -eq 0 ];then
+exit 1
+fi
