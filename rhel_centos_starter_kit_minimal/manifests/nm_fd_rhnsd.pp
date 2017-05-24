5eb64e1ff3cf09ef2aa3980a13ffa1dac81bc8c0
seif karoui <karouiseif@gmail.com>
2017-05-24 16:38:00 +0100

    new

--- rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
+++ rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
@@ -5,32 +5,32 @@ class rhel_centos_starter_kit_minimal::nm_fd_rhnsd(
 )
 {
 if($disable_nm == 'yes'){
-
-     file { '/tmp/disable_nm.sh':
-       ensure => file,
-       source => 'puppet:///modules/rhel_centos_starter_kit_minimal/nm_disable.sh',
-       mode   => 'u+x',
-       before => Exec['disable_nm']
+  
+ file { '/tmp/disable_nm.sh':
+  ensure => file,
+  source => 'puppet:///modules/rhel_centos_starter_kit_minimal/nm_disable.sh',
+  mode   => 'u+x',
+  before => Exec['disable_nm']
      }
-     exec { 'disable_nm':
-       command => '/tmp/disable_nm.sh',
-       before  => Exec['restart_network_service']
-     }
-     exec { 'restart_network_service':
-       command => '/usr/sbin/service network restart'
-     }
-     service { 'NetworkManager':
-       enable => mask;
-     }
-   }
-   if ($disable_firwalld == 'yes') {
-     service { 'firewalld':
-       enable => mask,
-     }
-   }
-   if ($disable_rhnsd == 'yes') {
-     service { 'rhnsd':
-       enable => mask,
-     }
-   }
+ exec { 'disable_nm':
+  command => '/tmp/disable_nm.sh',
+  before  => Exec['restart_network_service']
+ }
+ exec { 'restart_network_service':
+  command => '/usr/sbin/service network restart'
+ }
+ service { 'NetworkManager':
+  enable => mask;
+ }
+}
+if ($disable_firwalld == 'yes') {
+ service { 'firewalld':
+  enable => mask,
+ }
+}
+if ($disable_rhnsd == 'yes') {
+ service { 'rhnsd':
+  enable => mask,
+ }
+}
 }
