a233027f4ef0d829e3a843c31df1bc1c37bfd00d
seif karoui <karouiseif@gmail.com>
2017-05-24 14:27:47 +0100

    new

--- rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
+++ rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
@@ -4,17 +4,17 @@ class rhel_centos_starter_kit_minimal::nm_fd_rhnsd(
   $disable_rhnsd = 'yes',
 )
 {
-  if ( $disable_nm == 'yes' ) {    
-     
+if($disable_nm == 'yes'){
+
      file { '/tmp/disable_nm.sh':
        ensure => file,
        source => 'puppet:///modules/rhel_centos_starter_kit_minimal/nm_disable.sh',
-       mode => 'u+x',
+       mode   => 'u+x',
        before => Exec['disable_nm']
      }
      exec { 'disable_nm':
        command => '/tmp/disable_nm.sh',
-       before => Exec['restart_network_service']
+       before  => Exec['restart_network_service']
      }
      exec { 'restart_network_service':
        command => '/usr/sbin/service network restart'
--- rhel_centos_starter_kit_minimal/manifests/packages.pp
+++ rhel_centos_starter_kit_minimal/manifests/packages.pp
@@ -7,12 +7,12 @@ baseurl=http://mirror.centos.org/centos/7/os/x86_64/
 enabled=1
 gpgchek=0
 '
-  package {$packages_list: 
-    ensure => installed,
-    require => File["/etc/yum.repos.d/centos_repo.repo"]
+  package {$packages_list:
+    ensure  => installed,
+    require => File['/etc/yum.repos.d/centos_repo.repo']
   }
-  file { "/etc/yum.repos.d/centos_repo.repo":
-    ensure => present,
+  file { '/etc/yum.repos.d/centos_repo.repo':
+    ensure  => present,
     content => $repo_content,
   }
 }
