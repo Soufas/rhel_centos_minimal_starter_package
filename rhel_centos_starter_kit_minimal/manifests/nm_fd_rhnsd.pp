693b527d935a68f3da260bdf9c034db799cfb54d
seif karoui <karouiseif@gmail.com>
2017-05-24 12:22:03 +0100

    many

--- rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
+++ rhel_centos_starter_kit_minimal/manifests/nm_fd_rhnsd.pp
@@ -1,19 +1,19 @@
 class rhel_centos_starter_kit_minimal::nm_fd_rhnsd(
-  $disable_nm = "yes",
-  $disable_firwalld = "yes",
-  $disable_rhnsd = "yes",
+  $disable_nm = 'yes',
+  $disable_firwalld = 'yes',
+  $disable_rhnsd = 'yes',
 )
 {
-  if ( $disable_nm == "yes" ) {    
+  if ( $disable_nm == 'yes' ) {    
      
      file { '/tmp/disable_nm.sh':
        ensure => file,
        source => 'puppet:///modules/rhel_centos_starter_kit_minimal/nm_disable.sh',
-       mode => "u+x",
+       mode => 'u+x',
        before => Exec['disable_nm']
      }
      exec { 'disable_nm':
-       command => "/tmp/disable_nm.sh",
+       command => '/tmp/disable_nm.sh',
        before => Exec['restart_network_service']
      }
      exec{ 'restart_network_service':
@@ -23,12 +23,12 @@ class rhel_centos_starter_kit_minimal::nm_fd_rhnsd(
           enable => mask;
      }
    }
-   if ($disable_firwalld == "yes") {
+   if ($disable_firwalld == 'yes') {
      service { 'firewalld':
        enable => mask,
      }
    }
-   if ($disable_rhnsd == "yes") {
+   if ($disable_rhnsd == 'yes') {
      service { 'rhnsd':
        enable => mask,
      }
--- rhel_centos_starter_kit_minimal/manifests/packages.pp
+++ rhel_centos_starter_kit_minimal/manifests/packages.pp
@@ -1,12 +1,12 @@
 class rhel_centos_starter_kit_minimal::packages {
   $packages_list = [net-tools,vim-enhanced,nc,wireshark,tcpdump,wget,strace,lynx,sysstat,lsof,deltarpm,mlocate,bash-completion,nmap,telnet,lshw]
-  $repo_content="
+  $repo_content='
 [centos_repo]
 name= cent_os repo
 baseurl=http://mirror.centos.org/centos/7/os/x86_64/
 enabled=1
 gpgchek=0
-"
+'
   package {$packages_list: 
     ensure => installed,
     require => File["/etc/yum.repos.d/centos_repo.repo"]
